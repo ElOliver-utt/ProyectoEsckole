a53503b06b5bcd9b2a9c450e4c733ad2d14824e6
mauricio8507 <mauricio_azto@outlook.com>
2019-04-08 15:35:12 -0500

    Programación de Splash Cambio de color Agregado de Icono

--- app/src/main/java/com/example/proyectoesckole/MainActivity.java
+++ app/src/main/java/com/example/proyectoesckole/MainActivity.java
@@ -1,37 +1,62 @@
 package com.example.proyectoesckole;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.proyectoesckole.iFragments.iFragments;
+import com.example.proyectoesckole.cFragments.alumnos;
+import com.example.proyectoesckole.cFragments.calificaciones;
+import com.example.proyectoesckole.cFragments.contacto;
+import com.example.proyectoesckole.cFragments.notificaciones;
+import com.example.proyectoesckole.iFragments.iFragments;
+
+public class MainActivity extends AppCompatActivity implements iFragments {
 
     private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
+
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            Fragment fragment = new  alumnos();
+            getSupportFragmentManager().beginTransaction().replace(R.id.container,fragment).commit();
+            boolean ban = false;
+
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
+                    // mTextMessage.setText(R.string.title_home);
+                    fragment = new alumnos();
+                    return ban = true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
+                    //  mTextMessage.setText(R.string.title_dashboard);
+                    fragment = new calificaciones();
+                    return ban = true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
+                    // mTextMessage.setText(R.string.title_notifications);
+                    fragment = new notificaciones();
+                    return ban = true;
 
                 case R.id.navigation_contact:
-                    mTextMessage.setText("Contacto");
-                    return true;
+                    // mTextMessage.setText("Contacto");
+                    fragment = new contacto();
+                    return ban = true;
             }
-            return false;
+
+            if (ban)
+            {
+                getSupportFragmentManager().beginTransaction().replace(R.id.container,fragment).commit();
+            }
+
+            return true;
         }
     };
 
@@ -43,5 +68,10 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
---
+++ app/src/main/java/com/example/proyectoesckole/iFragments/iFragments.java
@@ -0,0 +1,9 @@
+package com.example.proyectoesckole.iFragments;
+
+import com.example.proyectoesckole.cFragments.alumnos;
+import com.example.proyectoesckole.cFragments.calificaciones;
+import com.example.proyectoesckole.cFragments.contacto;
+import com.example.proyectoesckole.cFragments.notificaciones;
+
+public interface iFragments extends alumnos.OnFragmentInteractionListener, calificaciones.OnFragmentInteractionListener, contacto.OnFragmentInteractionListener, notificaciones.OnFragmentInteractionListener  {
+}
